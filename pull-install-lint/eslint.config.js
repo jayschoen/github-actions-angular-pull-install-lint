d5ad88582a90224d9924e99d5f2c9c2878cd2371
Jay Schoen <jschoen@theswiftgroup.com>
2024-05-24 16:03:56 -0400

    change eslint config AGAIN

--- pull-install-lint/eslint.config.js
+++ pull-install-lint/eslint.config.js
@@ -1,41 +1,50 @@
-module.exports = {
-  overrides: [
-    {
-      files: ['*.ts'],
+import { typescript } from '@eslint/config';
+
+export default [
+  {
+    ignores: ['projects/**/*']
+  },
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
       parser: '@typescript-eslint/parser',
       parserOptions: {
-        project: 'tsconfig.json',
+        project: './tsconfig.json',
         createDefaultProgram: true,
       },
-      plugins: ['@typescript-eslint', '@angular-eslint'],
-      extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:@angular-eslint/recommended',
+    },
+    plugins: {
+      '@typescript-eslint': require('@typescript-eslint/eslint-plugin'),
+      '@angular-eslint': require('@angular-eslint/eslint-plugin'),
+    },
+    rules: {
+      ...typescript.configs.recommended.rules,
+      ...require('@angular-eslint/eslint-plugin').configs.recommended.rules,
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          vars: 'all',
+          args: 'after-used',
+          ignoreRestSiblings: false,
+        },
       ],
-      rules: {
-        '@typescript-eslint/explicit-function-return-type': 'off',
-        '@typescript-eslint/no-explicit-any': 'off',
-        '@typescript-eslint/no-unused-vars': [
-          'error',
-          {
-            vars: 'all',
-            args: 'after-used',
-            ignoreRestSiblings: false,
-          },
-        ],
-        quotes: ['error', 'single'],
-        semi: ['error', 'always'],
-      },
+      quotes: ['error', 'single'],
+      semi: ['error', 'always'],
     },
-    {
-      files: ['*.html'],
+  },
+  {
+    files: ['**/*.html'],
+    plugins: {
+      '@angular-eslint/template': require('@angular-eslint/eslint-plugin-template'),
+    },
+    languageOptions: {
       parser: '@angular-eslint/template-parser',
-      plugins: ['@angular-eslint/template'],
-      extends: ['plugin:@angular-eslint/template/recommended'],
-      rules: {
-        'max-len': ['error', { code: 140 }],
-      },
     },
-  ],
-};
+    rules: {
+      ...require('@angular-eslint/eslint-plugin-template').configs.recommended.rules,
+      'max-len': ['error', { code: 140 }],
+    },
+  },
+];
